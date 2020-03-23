49a089f1555a9e5b85f05a004d9d92d9527bf71f
eawang <eason.wang@montage-tech.com>
2020-03-23 05:56:28 -0400

    get the devices

--- get_kernel.c
+++ get_kernel.c
@@ -18,5 +18,24 @@ int main(void)
 		perror("clGetPlatformIDs failed!!!!\n");
 		exit(1);
 	}
+	//get the devices list and choose
+	cl_device_id *devices_list =NULL;
+	cl_uint       num_devices;
+	clStatus = clGetDeviceIDs(platforms[0],CL_DEVICE_TYPE_CPU,0,NULL,&num_devices);
+	if(clStatus < 0)
+	{
+		perror("clGetPlatformIDs step 1 failed\n");
+		exit(1);
+	}
+	devices_list = (cl_device_id *)malloc(sizeof(cl_device_id)*num_devices);
+	clStatus = clGetDeviceIDs(platforms[0],CL_DEVICE_TYPE_CPU,num_devices,devices_list,NULL);
+	if(clStatus < 0)
+	{
+		perror("clGetPlatformIDs step 1 failed\n");
+		exit(1);
+	}
+
+	printf("num_devices = %d\n",num_devices);
+
 
 }
