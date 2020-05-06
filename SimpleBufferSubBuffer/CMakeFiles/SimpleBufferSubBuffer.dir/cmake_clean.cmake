447481bbed820ec274a8e034f62489c3d4bc89bd
eawang <easonwang27@163.com>
2020-05-06 01:22:01 -0400

    subBuffer

---
+++ SimpleBufferSubBuffer/CMakeFiles/SimpleBufferSubBuffer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleBufferSubBuffer.dir/simple.cpp.o"
+  "SimpleBufferSubBuffer.pdb"
+  "SimpleBufferSubBuffer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleBufferSubBuffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
